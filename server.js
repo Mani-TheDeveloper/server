22110d574ce3b859ba03c22d09cb7d81d7c83a60
Mani-TheDeveloper <manishankar.ams@gmail.com>
2026-04-24 23:59:14 +0530

    Done

--- server.js
+++ server.js
@@ -3,28 +3,14 @@ const app = express();
 
 app.use(express.json());
 
-const port = 3000;
-const verifyToken = 12345;
 
 // ✅ GET
 app.get('/', (req, res) => {
-  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
-
-  if (mode === 'subscribe' && token === verifyToken) {
-    console.log('WEBHOOK VERIFIED');
-    res.status(200).send(challenge);
-  } else {
-    res.status(403).end();
-  }
+  console.log(req)
+  res.status(200).send(req.body);
 });
 
-// ✅ POST
-app.post('/', (req, res) => {
-  console.log("📩 Incoming:");
-  console.log(JSON.stringify(req.body, null, 2));
-  res.status(200).end();
-});
 
-app.listen(port, () => {
-  console.log(`Listening on ${port}`);
+app.listen(3000, () => {
+  console.log(`Listening on 3000`);
 });
